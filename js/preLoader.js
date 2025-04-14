d7e9df366cc840c09c22bb2ac2150d9460c1c773
Yahia-153 <yahiametwally153@gmail.com>
2025-04-14 17:24:28 +0200

    apdate

--- js/preLoader.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        document.querySelector('#preloader').classList.add('d-none');
-    }
-        , 0);
-})
-function animation() {
-
-}
--- js/qoute.js
+++
@@ -1,8 +0,0 @@
-fetch('https://dummyjson.com/quotes/random')
-.then(res => res.json())
-.then(data => {
-    const quote = data.quote;
-    const author = data.author;
-    document.getElementById("footer-quote").innerHTML = quote;
-    document.getElementById("footer-author").innerHTML = author;
-});
